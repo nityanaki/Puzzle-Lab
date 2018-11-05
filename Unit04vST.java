3892de402762e6084fc33c30d824af6f0aee414e
Nitya <nityanaki@gmail.com>
2018-11-05 02:44:33 -0600

    Update Unit04vST.java

--- Unit04vST.java
+++ Unit04vST.java
@@ -16,7 +16,6 @@ import java.util.Scanner;
 public class Unit04vST extends Applet
 {
 	
-	//Rectangle r11,r12,r13,r21,r22,r23,r31,r32,r33;
 	Rectangle[][] collection;
 	boolean scramble[];
 	String matrix[][];
@@ -30,16 +29,6 @@ public class Unit04vST extends Applet
 	
 	public void init()
 	{	
-		/*r11 = new Rectangle(100,100,200,200);
-		r12 = new Rectangle(300,100,200,200);
-		r13 = new Rectangle(500,100,200,200);
-		r21 = new Rectangle(100,300,200,200);
-		r22 = new Rectangle(300,300,200,200);
-		r23 = new Rectangle(500,300,200,200);
-		r31 = new Rectangle(100,500,200,200);
-		r32 = new Rectangle(300,500,200,200);
-		r33 = new Rectangle(500,500,200,200);*/
-		
 		Scanner keyboard = new Scanner(System.in);
 		System.out.println("Enter play level:");
 		System.out.println("3x3");
@@ -62,17 +51,6 @@ public class Unit04vST extends Applet
 			hundredcol=1;
 			hundredrow+=1;
 		}
-		
-		
-		
-		/*for (int r=0;r<3;r++)
-		{
-			g.drawLine((r+1)*100+100,100,(r+1)*100+100,500);
-		}
-		for (int c=0;c<3;c++)
-		{
-			g.drawLine(100,(c+1)*100+100,500,(c+1)*100+100);
-		}*/
 		
 		
 		matrix = new String[size+2][size+2];
@@ -122,15 +100,6 @@ public class Unit04vST extends Applet
 	public void paint(Graphics g)
 	{
 		drawGrid(g);
-		/*drawLetter(g,matrix[1][1],100,100);
-		drawLetter(g,matrix[1][2],300,100);
-		drawLetter(g,matrix[1][3],500,100);
-		drawLetter(g,matrix[2][1],100,300);		
-		drawLetter(g,matrix[2][2],300,300);
-		drawLetter(g,matrix[2][3],500,300);
-		drawLetter(g,matrix[3][1],100,500);
-		drawLetter(g,matrix[3][2],300,500);
-		drawLetter(g,matrix[3][3],500,500);*/
 		int hundredrow = 1;
 		int hundredcol = 1;
 		for (int r=1;r<matrix.length-1;r++)
@@ -148,16 +117,6 @@ public class Unit04vST extends Applet
 	
 	public void drawGrid(Graphics g)
 	{
-		//g.drawRect(100,100,600,600);
-		//new code is line below
-		/*g.drawRect(100,100,800,800);
-		g.drawLine(300,100,300,900);
-		g.drawLine(500,100,500,900);
-		g.drawLine(100,300,900,300);
-		g.drawLine(100,500,900,500);
-		//new code
-		g.drawLine(700,100,700,900);
-		g.drawLine(100,700,900,700);*/
 		g.drawRect(100,100,size*100,size*100);
 		for (int r=0;r<size-1;r++)
 		{
@@ -190,24 +149,6 @@ public class Unit04vST extends Applet
 	
 	public boolean mouseDown(Event e, int x, int y)
 	{
-		/*if(r11.inside(x,y) && okSquare(1,1))
-			swap(1,1);
-		else if(r12.inside(x,y) && okSquare(1,2))
-			swap(1,2);
-		else if(r13.inside(x,y) && okSquare(1,3))
-			swap(1,3);
-		else if(r21.inside(x,y) && okSquare(2,1))
-			swap(2,1);
-		else if(r22.inside(x,y) && okSquare(2,2))
-			swap(2,2);
-		else if(r23.inside(x,y) && okSquare(2,3))
-			swap(2,3);
-		else if(r31.inside(x,y) && okSquare(3,1))
-			swap(3,1);
-		else if(r32.inside(x,y) && okSquare(3,2))
-			swap(3,2);
-		else if(r33.inside(x,y) && okSquare(3,3))
-			swap(3,3);*/
 		for (int i=1;i<matrix.length-1;i++)
 		{
 			for (int j=1;j<matrix[i].length-1;j++)
